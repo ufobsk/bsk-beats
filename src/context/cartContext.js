a7889bec7e7a9b89d6e9c71189e7ffd66b3d0edf
fnkbsk <igoico10@gmail.com>
2023-05-11 23:51:15 -0300

    comentarios

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -8,3 +8,33 @@ export const CartProvider = ({ children }) => (
         ({children})
     </cartContext.Provider>
 );
+
+
+/* Metodos recomendados para el Cart Context
+
+addItem(item, quantity) => agregar cierta cantidad d un ítem al carrito
+removeItem(itemId) => remover un item del cart por usando su id
+clear() => remover todos los items
+islnCart: (id) => true|false */
+
+/* Ejemplo?
+
+import { createContext, useState } from 'react';
+export const CartContext = createContext();
+const CartContextProvider = ({ childrem }) => {
+    const [cartList, setCartList] = useState([]);
+    const addToCart = (item, quantity) => { implementa la funcionalidad para agregar un producto al carrito
+    } 
+    const removeList = () { implementa la funcionalidad para dejar el carrito vacio
+    }
+    const deleteItem = (id) => { implementa la funcionalidad para borrar un producto del carrito
+    }
+    return (
+        <CartContext.Provider value={{carList, addToCart, removeList, deleteItem}}>
+            { children }
+        </CartContext.Provider>
+    );
+}
+export default CartContextProvider;
+
+*/
