9f43fac5efbff7928d54db24b5a9167726bf3bda
fnkbsk <igoico10@gmail.com>
2023-05-18 19:38:58 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from './context/cartContext';
+
+// import { getFirestore, getDoc, doc } from 'firebase/firestore'
 
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
           <Route path="/" element= {<ItemListContainer/>} />
           <Route path="/category/:categoryId" element= {<ItemListContainer/>} />
           <Route path="/item/:itemId" element= {<ItemDetailContainer/>} />
+          <Route path="/cart" element= {<Cart/>} />
           <Route path="*" element={<h1>404 NOT FOUND</h1>}/>
         </Routes>
       </CartProvider>
