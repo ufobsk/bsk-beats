65e92c83ba0463da342c1ce2c330ee71bb867476
fnkbsk <igoico10@gmail.com>
2023-06-02 12:36:35 -0300

    Entrega-Proyecto-Final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,34 +1,49 @@
-import { useContext } from 'react';
-import { CartContext } from '../../context/CartContext';
-import CartItem from '../CartItem/CartItem';
-import { Link } from 'react-router-dom';
-import './Cart.css';
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import CartItem from "../CartItem/CartItem";
+import { Link } from "react-router-dom";
+import "./Cart.css";
 
 const Cart = () => {
-    const { cart, clearCart, totalQuantity, total, removeItem } = useContext(CartContext)
+  const { cart, clearCart, totalQuantity, total, removeItem } =
+    useContext(CartContext);
 
-    if(totalQuantity === 0) {
-        return(
-            <div>
-                <h3>No tenés productos agregados al carrito</h3>
-                <Link to='/' className='Option'>Productos</Link>
-            </div>
-        )
-    }
+  const redirect = () => {
+    setTimeout(() => {
+      window.location = origin;
+    }, 2000);
+  };
 
-    const handleRemoveItem = (itemId) => {
-        removeItem(itemId);
-    };
-
+  if (totalQuantity === 0) {
     return (
-        <div>
-            <h3>Resumen de tu compra</h3>
-            {cart.map(p =>  <CartItem key={p.id}{...p} handleRemoveItem={handleRemoveItem}/>)}
-            <h4>Total: ${total}</h4>
-            <button onClick={() => clearCart()} className='Option'>Limpiar Carrito</button>
-            <Link to='/checkout' className='Option'>Ir a pagar</Link>
-        </div>
-    )
-}
+      <div>
+        <h3>No tenés productos agregados al carrito, redireccionando al inicio...</h3>
+        {
+            redirect()
+        }
+      </div>
+    );
+  }
 
-export default Cart
+  const handleRemoveItem = (itemId) => {
+    removeItem(itemId);
+  };
+
+  return (
+    <div>
+      <h3>Resumen de tu compra</h3>
+      {cart.map((p) => (
+        <CartItem key={p.id} {...p} handleRemoveItem={handleRemoveItem} />
+      ))}
+      <h4>Total: ${total}</h4>
+      <button onClick={() => clearCart()} className="Option">
+        Limpiar Carrito
+      </button>
+      <Link to="/checkout" className="Option">
+        Ir a pagar
+      </Link>
+    </div>
+  );
+};
+
+export default Cart;
